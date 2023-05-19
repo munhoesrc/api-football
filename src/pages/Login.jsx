38879730b39fb6f3cd956b29e77da58d34bf0859
Renan Casarin Munhoes <munhoesrc@gmail.com>
2023-05-19 01:12:29 -0300

    feat: Adicionando img no Login e estilizando

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,52 +1,66 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import WalletAnimate from "../assets/img/Soccer.png";
+import "../styles/login.css";
 
 function Login() {
   const navigate = useNavigate();
 
-  const [apiKey, setApiKey] = useState('');
-  const [error, setError] = useState('');
+  const [apiKey, setApiKey] = useState("");
+  const [error, setError] = useState("");
 
   const handleLogin = async (e) => {
     e.preventDefault();
 
     // Verifica se a API key está preenchida
     if (!apiKey) {
-      setError('Por favor, insira uma API key válida.');
+      setError("Por favor, insira uma API key válida.");
       return;
     }
 
+    // Verifica autenticidade da API key
     try {
-      // Verifica autenticidade da API key
-      const response = await fetch('https://v3.football.api-sports.io/status', {
+      const response = await fetch("https://v3.football.api-sports.io/status", {
         headers: {
-          'x-apisports-key': apiKey
-        }
+          "x-apisports-key": apiKey,
+        },
       });
 
       if (response.ok) {
-        navigate('/home');
+        navigate("/home");
       } else {
-        setError('API key inválida. Por favor, verifique novamente.');
+        setError("API key inválida. Por favor, verifique novamente.");
       }
     } catch (error) {
-      setError('Ocorreu um erro ao verificar a API key. Por favor, tente novamente mais tarde.');
+      setError(
+        "Ocorreu um erro ao verificar a API key. Por favor, tente novamente mais tarde."
+      );
     }
   };
 
   return (
-    <div>
-      <h1>Login</h1>
-      {error && <p>{error}</p>}
-      <form onSubmit={handleLogin}>
-        <input
-          type="text"
-          placeholder="Digite sua API key"
-          value={apiKey}
-          onChange={(e) => setApiKey(e.target.value)}
-        />
-        <button type="submit">Login</button>
-      </form>
+    <div className="main-login">
+      <div className="left-login">
+        <img src={WalletAnimate} className="login-image" alt="jogando bola" />
+      </div>
+      <div className="right-login">
+        <div className="card-login">
+          <h1>LOGIN</h1>
+          <br />
+          {error && <p>{error}</p>}
+          <form onSubmit={handleLogin}>
+            <input
+              type="text"
+              placeholder="Digite sua API key"
+              value={apiKey}
+              onChange={(e) => setApiKey(e.target.value)}
+            />
+            <button type="submit" className="btn-login">
+              Entrar
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
