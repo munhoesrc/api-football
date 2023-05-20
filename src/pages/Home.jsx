9c2c051940dea55365505159fcd0d2d753cba63e
Renan Casarin Munhoes <munhoesrc@gmail.com>
2023-05-20 17:12:37 -0300

    style: Estilização da Home, organização dos cards e CSS in live

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import useGetData from "../services/requests";
 import "../styles/home.css";
+import "../styles/live.css";
 
 function Home() {
   const API_LIVE = "https://v3.football.api-sports.io/fixtures?live=all";
@@ -15,46 +16,51 @@ function Home() {
       setPartidas(partidasData);
     }
   }, [partidasData]);
-    
+
   return (
     <div>
       <Navbar />
       <header className="App-header">
-        <h1 className="title-home">Partidas ao vivo</h1>
-        {partidas ? (
-          partidas.map((partida) => (
-            <div className="container-home" key={partida.fixture.id}>
-              <div className="team-home">
-                <h1>{partida.teams.home.name}</h1>
-                <br />
-                <img
-                  src={partida.teams.home.logo}
-                  className="img-team"
-                  alt="Equipe local"
-                />
-                <h2 className="score"> {partida.goals.home} </h2>
-              </div>
-              <div className="div-vs">
-                <h1>VS</h1>
-                <p> {partida.fixture.status.elapsed} ' </p>
-              </div>
-              <div className="team-away">
-                <h1>{partida.teams.away.name}</h1>
-                <br />
-                <img
-                  src={partida.teams.away.logo}
-                  className="img-team"
-                  alt="Equipe visitante"
-                />
-                <h2 className="score">{partida.goals.away}</h2>
-              </div>
+        <h1 className="title-home">Partidas ao vivo</h1><div class="circle"></div>
+        <div className="container-wrapper">
+          {partidas && partidas.length > 0 && (
+            <div className="row">
+              {partidas.map((partida, index) => (
+                <div className="container-home" key={partida.fixture.id}>
+                  <div className="team-home">
+                    <h1>{partida.teams.home.name}</h1>
+                    <br />
+                    <img
+                      src={partida.teams.home.logo}
+                      className="img-team"
+                      alt="Equipe local"
+                    />
+                    <h2 className="score"> {partida.goals.home} </h2>
+                  </div>
+                  <div className="div-vs">
+                    <h1>VS</h1>
+                    <p> {partida.fixture.status.elapsed} ' </p>
+                  </div>
+                  <div className="team-away">
+                    <h1>{partida.teams.away.name}</h1>
+                    <br />
+                    <img
+                      src={partida.teams.away.logo}
+                      className="img-team"
+                      alt="Equipe visitante"
+                    />
+                    <h2 className="score">{partida.goals.away}</h2>
+                  </div>
+                </div>
+              ))}
             </div>
-          ))
-        ) : null}
+          )}
+        </div>
       </header>
       <Footer />
     </div>
   );
+  
 }
 
 export default Home;
